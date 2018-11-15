ebfb6213c59a32c5bc6d2546b1bd85c6589b49d0
QuentinJanuel <facemashldv@laposte.net>
2018-11-15 19:53:04 +0100

    WIP again

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+let size = 600;
+let scalar2D = 1/2;
+
 function newColor(red, green, blue){
 	return {
 		r: red, g: green, b: blue
@@ -32,76 +35,131 @@ points.G = barycenter(points.A, points.B, points.C);
 // The green point
 points.D = newPoint(-75, 0, 50, newColor(0, 255, 0));
 
+// 2D Canvas
+let canvas2D = document.createElement("canvas");
+canvas2D.setAttribute("width", size*scalar2D);
+canvas2D.setAttribute("height", size*scalar2D);
+document.body.appendChild(canvas2D);
+let ctx2D = canvas2D.getContext("2d");
+ctx2D.font = "20px Arial"
+let clearCanvas2D = function(){
+	ctx2D.fillStyle = "#FFF";
+	ctx2D.fillRect(0, 0, canvas2D.width, canvas2D.height);
+}
+let draw2DPoints = function(){
+	clearCanvas2D();
+	for(key of ['A' ,'B', 'C', 'D', 'G']){
+		let x = canvas2D.width/2+points[key].x*scalar2D;
+		let y = canvas2D.height/2+points[key].z*scalar2D;
+		if(points[key].color)
+			ctx2D.fillStyle = "rgb("+points[key].color.r+", "+points[key].color.g+", "+points[key].color.b+")";
+		else
+			ctx2D.fillStyle = "#F00";
+		ctx2D.beginPath();
+		ctx2D.arc(x, y, 5, 0, 2*Math.PI);
+		ctx2D.fill();
+		ctx2D.closePath();
+		ctx2D.fillText(key, x+10, y+10);
+	}
+}
+draw2DPoints();
+canvas2D.addEventListener("click", function(e){
+	points.D.x = (e.clientX-canvas2D.width/2)/scalar2D;
+	points.D.z = (e.clientY-canvas2D.height/2)/scalar2D;
+	draw2DPoints();
+});
+
 // Outer points
-let size = 200;
-points.x1 = newPoint(-size, 0, -size, newColor(100, 100, 100));
-points.x2 = newPoint(-size, 0, +size, newColor(100, 100, 100));
-points.x3 = newPoint(+size, 0, +size, newColor(100, 100, 100));
-points.x4 = newPoint(+size, 0, -size, newColor(100, 100, 100));
+points.x1 = newPoint(-size/2, 0, -size/2, newColor(100, 100, 100));
+points.x2 = newPoint(-size/2, 0, +size/2, newColor(100, 100, 100));
+points.x3 = newPoint(+size/2, 0, +size/2, newColor(100, 100, 100));
+points.x4 = newPoint(+size/2, 0, -size/2, newColor(100, 100, 100));
 
 // Camera
 let cam = newPoint(0, 0, 0);
 let oldMouse = {x: 0, y: 0};
-let dist;
+let camDist = 600;
+let camTheta1 = Math.PI*2/3;
+let camTheta2 = Math.PI/2;
 
-// Y Scalar
+// Variables
 let yScalar = 150;
+let mousePressedCanvas = false;
+let graphics;
 
 // angles for moving the point A (temp)
 let angleA = 0, angleB = 0;
 
 function setup(){
-	createCanvas(window.innerWidth, window.innerHeight, WEBGL);
-	cam.y = -300;
-	cam.z = (height/2)/Math.tan(Math.PI/6);
-	textFont(loadFont(
-		"https://fonts.gstatic.com/s/newscycle/v14/CSR54z1Qlv-GDxkbKVQ_dFsvWNRevA.ttf"
-	));
-	textSize(15);
-	textAlign(CENTER, CENTER);
+	let canvas = createCanvas(window.innerWidth-canvas2D.width, window.innerHeight, WEBGL);
+	canvas.mousePressed(function(){
+		mousePressedCanvas = true;
+	}).mouseReleased(function(){
+		mousePressedCanvas = false;
+	});
+	graphics = createGraphics(300, 300);
+	graphics.textAlign(CENTER, CENTER);
+	graphics.textSize(200);
 }
 
 function moveInSpace(){
-	dist = Math.sqrt(cam.x*cam.x+cam.y*cam.y+cam.z*cam.z);
-	if(mouseIsPressed){
-		let newCam = {
-			x: cam.x+oldMouse.x-mouseX,
-			y: cam.y+oldMouse.y-mouseY
-		};
-		let idk = dist*dist-newCam.x*newCam.x-newCam.y*newCam.y;
-		if(idk >= 0){
-			cam.x = newCam.x;
-			cam.y = newCam.y;
-			cam.z = Math.sqrt(idk);
-		}
+	if(mousePressedCanvas){
+		camTheta2 += (mouseX-oldMouse.x)*Math.PI/180/3;
+		camTheta1 += (mouseY-oldMouse.y)*Math.PI/180/3;
+		let epsilon = .01;
+		camTheta1 = Math.max(epsilon, Math.min(camTheta1, Math.PI-epsilon));
 	}
 	oldMouse = {x: mouseX, y: mouseY};
+	cam.x = camDist*Math.sin(camTheta1)*Math.cos(camTheta2);
+	cam.z = camDist*Math.sin(camTheta1)*Math.sin(camTheta2);
+	cam.y = camDist*Math.cos(camTheta1);
 	camera(cam.x, cam.y, cam.z, 0, 0, 0, 0, 1, 0);
+}
+
+function mouseWheel(e){
+	camDist = Math.min(1000, Math.max(400, camDist+e.delta/3));
 }
 
 function initSpace(){
 	background(0);
 	ambientLight(255);
 	noStroke();
+	moveInSpace();
 }
 
 function drawPoint(point, name){
-	if(point.color)
+	if(point.color){
+		graphics.fill(point.color.r, point.color.g, point.color.b);
 		ambientMaterial(point.color.r, point.color.g, point.color.b);
-	else
+	}else{
+		graphics.fill(255, 0, 0);
 		ambientMaterial(255, 0, 0);
+	}
+	// Draw the point
 	translate(point.x, point.y, point.z);
 	sphere(5);
 	translate(-point.x, -point.y, -point.z);
+	//
+	// Draw text point
 	let scal = 1/2;
+	let up = 10;
 	let textPoint = newPoint(
 		cam.x+(point.x-cam.x)*scal,
-		cam.y+(point.y-cam.y)*scal,
+		cam.y+(point.y-up-cam.y)*scal,
 		cam.z+(point.z-cam.z)*scal
 	);
-	translate(textPoint.x, textPoint.y, textPoint.z);
-	text(name, 0, 0);
-	translate(-textPoint.x, -textPoint.y, -textPoint.z);
+	translate(textPoint.x, textPoint.y-up, textPoint.z);
+	rotateY(-camTheta2+Math.PI/2);
+	rotateX(camTheta1-Math.PI/2);
+	graphics.clear();
+	graphics.background(5, 4, 68);
+	graphics.text(name, 150, 150);
+	texture(graphics);
+	plane(20);
+	rotateX(-camTheta1+Math.PI/2);
+	rotateY(camTheta2-Math.PI/2);
+	translate(-textPoint.x, -textPoint.y+up, -textPoint.z);
+	// Set back the ambient material
 	ambientMaterial(255);
 }
 
@@ -181,6 +239,4 @@ function draw(){
 	drawBottomPlane();
 	drawABCPlane();
 	drawTopPlane();
-	// Move
-	moveInSpace();
 }
